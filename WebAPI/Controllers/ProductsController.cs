a753d94f269a5957d194ac5dc2275b0f0c199281
Kutluhan Akarcesme <kutluhanakarcesme@gmail.com>
2022-08-01 15:48:49 +0300

    Projeye WebAPI katmanının eklenmesi

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramewrok;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase 
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IDataResult<List<Product>> Get()
+        {
+            
+
+            return new SuccessDataResult<List<Product>>(_productService.GetAll().Data);
+        }
+    }
+}
