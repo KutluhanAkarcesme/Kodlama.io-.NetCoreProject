9a27ef87d6780cd7624fbf72b4409697cfdd08ad
KutluhanAkarcesme <kutluhanakarcesme@gmail.com>
2022-10-10 10:58:56 +0300

    IQueryableRepository added

---
+++ Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        private IDbSet<T> _entities;
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+
+        protected virtual IDbSet<T> Entities
+        {
+            get 
+            {
+                if (_entities == null)
+                {
+                    _entities = _context.Set<T>();
+                }
+                return _entities;
+            }
+        }
+    }
+}
